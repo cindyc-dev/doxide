3d10b242a939f62c47d9ece9664f4d94053d91ef
chuahx <xinyu.chuah@student.unimelb.edu.au>
2022-03-24 17:40:01 +1100

    Added CodeLens (appears at the start of every function)

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,14 @@
 import { commands, Disposable, window, languages, workspace, ProgressLocation } from "vscode";
+import { DoxideCodeLensProvider } from "./CodeLensProvider";
 
 let disposables: Disposable[] = [];
 
 export function activate() {
 	window.showInformationMessage(`🤖 Doxide extension is activated!`);
 	
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+	const authKey: string | undefined = workspace
+		.getConfiguration("doxide")
+		.get("doxide.openAI.apiKey");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,6 +18,25 @@ export function activate(context: vscode.ExtensionContext) {
 	// TODO yeet this - Command to say hello
 	commands.registerCommand("doxide.helloWorld", (name?) => {
 		window.showInformationMessage(`Hello ${name ? name : ""} from Doxide!`);
+	});
+
+	/* ------------------------------ CodeLens ------------------------------ */
+	// Create and register CodeLensProvider (only for Python, for now)
+	const codeLensProvider = new DoxideCodeLensProvider();
+	languages.registerCodeLensProvider("python", codeLensProvider);
+
+	// Command to enable CodeLenses
+	commands.registerCommand("doxide.enableCodeLens", () => {
+		console.log("[Command] doxide.enableCodeLens called.");
+		workspace.getConfiguration("doxide").update("codeLens.enabled", true);
+		window.showInformationMessage(`✅ Doxide: CodeLens Enabled.`);
+	});
+
+	// Command to disable CodeLenses
+	commands.registerCommand("doxide.disableCodeLens", () => {
+		console.log("[Command] doxide.disableCodeLens called.");
+		workspace.getConfiguration("doxide").update("codeLens.enabled", false);
+		window.showInformationMessage(`✅ Doxide: CodeLens Disabled.`);
 	});
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
